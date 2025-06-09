400c33d405ca568d0119d74d8d57a8771444122e
maike842 <maikeking@protonmail.com>
2025-06-10 03:36:13 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -210,7 +210,7 @@ export default {
     showUploadPopup: false,
     uploadProgress: null,
     uploadQueue: [],
-    backgroundImageUrl: "/assets/bg-light.webp"
+    backgroundImageUrl: "/assets/background1.jpg"
   }),
 
   computed: {
